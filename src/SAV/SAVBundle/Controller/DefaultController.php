592be3cfd28bfb0fc9316a59166ae85ad198ccd3
TarekBenYahia <60258461+TarekBenYahia@users.noreply.github.com>
2020-02-24 13:19:33 +0100

    Integration boutique 24/02 Mailing

--- src/SAV/SAVBundle/Controller/DefaultController.php
+++ src/SAV/SAVBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SAVBundle:Default:index.html.twig');
+        return $this->render('@SAV/Default/index.html.twig');
     }
 }
